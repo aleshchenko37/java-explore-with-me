075373ae97aa3904615313ff8c8661e67b7c5e78
Anastasiya <127428946+aleshchenko37@users.noreply.github.com>
2023-12-13 12:10:12 +0300

    poms corrected

--- stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.ewm.dto.EndpointHit;
@@ -32,7 +33,7 @@ public class StatsClient {
         this.serverUrl = serverUrl;
     }
 
-    public EndpointHit saveHit(EndpointHit endpointHitDto) {
+    public ResponseEntity<EndpointHit> saveHit(EndpointHit endpointHitDto) {
         URI uri = URI.create(serverUrl + "/hit");
         final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(gson.toJson(endpointHitDto));
         HttpRequest request = HttpRequest.newBuilder()
@@ -42,13 +43,13 @@ public class StatsClient {
                 .build();
         try {
             final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return gson.fromJson(response.body(), EndpointHit.class);
-        } catch (NullPointerException | IOException | InterruptedException e) {
+            return ResponseEntity.ok(gson.fromJson(response.body(), EndpointHit.class));
+        } catch (IOException | InterruptedException e) {
             throw new ClientException("Ошибка в клиенте статистики при выполнении запроса: " + request);
         }
     }
 
-    public List<ViewStats> getAllStats(String start, String end,
+    public ResponseEntity<List<ViewStats>> getAllStats(String start, String end,
                                        List<String> uris, Boolean unique) {
         URI uri = UriComponentsBuilder.fromUriString(serverUrl)
                 .path("/stats")
@@ -69,8 +70,8 @@ public class StatsClient {
             final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             Type userType = new TypeToken<List<ViewStats>>() {
             }.getType();
-            return gson.fromJson(response.body(), userType);
-        } catch (NullPointerException | IOException | InterruptedException e) {
+            return ResponseEntity.ok(gson.fromJson(response.body(), userType));
+        } catch (IOException | InterruptedException e) {
             throw new ClientException("Ошибка в клиенте статистики при выполнении запроса: " + request);
         }
     }
