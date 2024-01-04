dccb1a2d5fff3a0720ca6fdb15d8b0bf93e67dc8
Anastasiya <127428946+aleshchenko37@users.noreply.github.com>
2024-01-04 14:45:57 +0300

    fix: add location additional functions

--- ewm-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
@@ -52,7 +52,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("select e " +
             "from Event e " +
             "where (e.state = 'PUBLISHED') " +
-            "and (function('distance', e.location.lat, e.location.lon, :lat, :lon) <= :radius) ")
+            "and (function('distance', e.location.lat, e.location.lon, :lat, :lon) <= :radius) " +
+            "and (e.eventDate BETWEEN rangeStart AND rangeEnd) ")
     List<Event> getAllEventsByLocation(
             @Param("lat") Float lat,
             @Param("lon") Float lon,
